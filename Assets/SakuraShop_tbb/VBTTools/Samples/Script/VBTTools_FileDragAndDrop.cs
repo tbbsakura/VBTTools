532687238a7eead323723e8bf68646362f3c1259
tbbsakura <tbbsakura1@gmail.com>
2024-10-21 12:06:16 +0900

    VRM1対応

--- Assets/SakuraShop_tbb/VBTTools/Samples/Script/VBTTools_FileDragAndDrop.cs
+++ Assets/SakuraShop_tbb/VBTTools/Samples/Script/VBTTools_FileDragAndDrop.cs
@@ -26,7 +26,10 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
+using System;
 
+using System.Threading;
+using System.Threading.Tasks;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -34,6 +37,7 @@ using B83.Win32;
 
 using System.IO;
 using VRM;
+using UniVRM10;
 using UniGLTF;
 using SakuraScript.VBTTool;
 
@@ -48,7 +52,15 @@ public class VBTTools_FileDragAndDrop : MonoBehaviour
 
     [SerializeField] EVMC4U.ExternalReceiver m_exrec;
     RuntimeGltfInstance _lastLoaded = null;
+    GameObject _lastLoaded10 = null;
     [SerializeField]  VBTToolsSample m_sampleProject;
+
+    SynchronizationContext synchronizationContext;
+
+    void Start()
+    {
+        synchronizationContext = SynchronizationContext.Current;
+    }
 
     void OnEnable ()
     {
@@ -85,11 +97,57 @@ public class VBTTools_FileDragAndDrop : MonoBehaviour
         if ( m_loading ) return; // ignore
         LoadModel(aFiles[0].ToString()); // load only 1st file
     }
-    
+
+    void DestroyLastLoaded()
+    {
+        if ( _lastLoaded != null ) {
+            Destroy(_lastLoaded.Root);
+            _lastLoaded = null;
+        }
+        if ( _lastLoaded10 != null) {
+            // VRM 10 のunloadをここで
+            Destroy(_lastLoaded10);          
+            _lastLoaded10 = null;  
+        }
+        
+    }
+
+    void OnLoaded10(GameObject loaded, string path)
+    {
+        if ( loaded == null )  return;
+        DestroyLastLoaded();
+        _lastLoaded10 = loaded;
+        loaded.transform.SetParent(transform, false);// このcomponentがattachされているオブジェクトを親に設定する
+        loaded.transform.position.Set(-1,1,3); // 位置調整
+        m_topText.text = "Showing Meshes...";
+
+        foreach (var spring in loaded.GetComponentsInChildren<VRMSpringBone>())
+        {
+            spring.Setup();
+            spring.gameObject.SetActive(false);
+        }
+
+        var meshes = loaded.GetComponentsInChildren<SkinnedMeshRenderer>();
+        foreach (var m in meshes) { 
+            m.updateWhenOffscreen = true;
+        }
+
+        Animator animator = loaded.GetComponent<Animator>();
+
+        if (animator != null)
+        {
+            if ( m_exrec != null ) m_exrec.Model = loaded;
+            m_lastLoadedFile = path; // load と show が成功してから更新する
+            m_sampleProject?.OnVRMLoaded(animator);
+        }
+        m_topText.text = "VRM1 loaded";
+    }
+
+
     void OnLoaded(RuntimeGltfInstance loaded, string path)
     {
         if ( loaded == null || loaded.Root == null )  return;
-        if ( _lastLoaded != null ) Destroy(_lastLoaded.Root);
+        DestroyLastLoaded();
         _lastLoaded = loaded;
         loaded.Root.transform.SetParent(transform, false);// このcomponentがattachされているオブジェクトを親に設定する
         loaded.Root.transform.position.Set(-1,1,3); // 位置調整
@@ -118,7 +176,7 @@ public class VBTTools_FileDragAndDrop : MonoBehaviour
                 m_sampleProject?.OnVRMLoaded(animator);
             }
         }
-        if (m_topText != null) m_topText.text = "VRM loaded";
+        if (m_topText != null) m_topText.text = "VRM0 loaded";
     }
 
     private async void LoadModel(string path)
@@ -126,59 +184,96 @@ public class VBTTools_FileDragAndDrop : MonoBehaviour
         var ext = Path.GetExtension(path).ToLower();
         if ( ext == ".vrm" ) 
         {
+            bool try10 = false;
             if ( m_topText != null ) m_topText.text = "Loading VRM...";
-            using ( var data = new GlbFileParser(path).Parse() ) 
+            // VRM0
+            try
             {
-                var vrm = new VRMData(data);
-                using ( var context = new VRMImporterContext(vrm) ) 
+                using ( var data = new GlbFileParser(path).Parse() ) 
                 {
-                    var loaded = default(RuntimeGltfInstance);
-                    m_loading = true;
-                    if (m_loadAsync)
+                    var vrm = new VRMData(data);
+                    using ( var context = new VRMImporterContext(vrm) ) 
                     {
-                        try {
-                            loaded = await context.LoadAsync(new VRMShaders.RuntimeOnlyAwaitCaller());
-                        } catch(System.Exception e) {
-                            System.Type exceptionType = e.GetType();
-                            if (m_topText != null) m_topText.text = "Load failed. : " + e.Message;
-                            m_loading = false;
-                            return;
+                        var loaded = default(RuntimeGltfInstance);
+                        m_loading = true;
+                        if (m_loadAsync)
+                        {
+                            try {
+                                loaded = await context.LoadAsync(new VRMShaders.RuntimeOnlyAwaitCaller());
+                            } catch(System.Exception e) {
+                                System.Type exceptionType = e.GetType();
+                                if (m_topText != null) m_topText.text = "Load failed. : " + e.Message;
+                                m_loading = false;
+                                return;
+                            }
                         }
-                    }
-                    else
-                    {
-                        try {
-                            loaded = context.Load();
-                        } catch(System.AggregateException ae1) { // 2層Aggregateが帰ってきたりするので
-                            foreach ( var eInner in ae1.InnerExceptions ) 
-                            {
-                                if (eInner.GetType() == typeof(System.AggregateException) ) 
+                        else
+                        {
+                            try {
+                                loaded = context.Load();
+                            } catch(System.AggregateException ae1) { // 2層Aggregateが帰ってきたりするので
+                                foreach ( var eInner in ae1.InnerExceptions ) 
                                 {
-                                    System.AggregateException ae2 = (System.AggregateException)eInner;
-                                    foreach ( var eInner2 in ae2.InnerExceptions ) 
+                                    if (eInner.GetType() == typeof(System.AggregateException) ) 
                                     {
-                                        if (m_topText != null) m_topText.text = "Load failed. : " + eInner2.Message;
+                                        System.AggregateException ae2 = (System.AggregateException)eInner;
+                                        foreach ( var eInner2 in ae2.InnerExceptions ) 
+                                        {
+                                            if (m_topText != null) m_topText.text = "Load failed. : " + eInner2.Message;
+                                            m_loading = false;
+                                            return;
+                                        }
+                                    }
+                                    else {
+                                        if (m_topText != null) m_topText.text = "Load failed. : " + eInner.Message;
                                         m_loading = false;
                                         return;
                                     }
                                 }
-                                else {
-                                    if (m_topText != null) m_topText.text = "Load failed. : " + eInner.Message;
-                                    m_loading = false;
-                                    return;
-                                }
+                                m_loading = false;
+                                return;
+                            } catch (System.Exception e) {
+                                if (m_topText != null) m_topText.text = "Load failed. : " + e.Message;
+                                m_loading = false;
+                                return;
                             }
-                            m_loading = false;
-                            return;
-                        } catch (System.Exception e) {
-                            if (m_topText != null) m_topText.text = "Load failed. : " + e.Message;
-                            m_loading = false;
-                            return;
                         }
+                        OnLoaded(loaded, path);
                     }
-                    OnLoaded(loaded, path);
                 }
             }
+            catch (NotVrm0Exception e)
+            {
+                Debug.Log("NotVrm0Exception: Try to load VRM1");
+                try10 = true;
+                //continue loading
+            }
+
+            // VRM1 : MCP2VMCP のソース例を真似
+            if ( try10 ) {
+                try
+                {
+                    string vrmfilepath = path;
+                    byte[] VRMdataRaw = File.ReadAllBytes(path);
+                    GlbLowLevelParser glbLowLevelParser = new GlbLowLevelParser(null, VRMdataRaw);
+                    GltfData gltfData = glbLowLevelParser.Parse();
+
+                    synchronizationContext.Post(async (_) =>
+                    {
+                        var instance = await Vrm10.LoadBytesAsync(VRMdataRaw, canLoadVrm0X: false, showMeshes: true);
+                        await Task.Delay(100); //VRM1不具合対策
+
+                        OnLoaded10(instance.gameObject, path);
+                    }, null);
+
+                    gltfData?.Dispose();
+                }
+                catch (Exception e)
+                {
+                    Debug.Log("VRM1 Exception: " +  e.Message  );
+                }
+            }
+            Debug.Log("Load Model done");
         }
         else {
             if (m_topText != null) m_topText.text = "Please open .vrm file.";
